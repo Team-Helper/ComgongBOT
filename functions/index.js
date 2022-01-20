7c275ef73ca0340c3caa44c4c44b07a7671573ae
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-01-20 22:26:29 +0900

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,18 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
 admin.initializeApp();
 const app = express();
 
-const HW = require('./router/HelloWorld')
+const HelloWorld = require('./router/HelloWorld');
 
 app.use(cors());
-app.use('/hw', HW);
+app.use('/', HelloWorld);
 
 exports.middleWare = functions
     .region('asia-northeast1')
     .https
     .onRequest(app);
+
+// module.exports = app;
