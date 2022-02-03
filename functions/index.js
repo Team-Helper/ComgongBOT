a27036e5a06851e47186819174fc6e6e7d328f8e
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-02-03 16:46:28 +0900

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,11 @@ require('dotenv').config();
 admin.initializeApp({
     credential: admin
         .credential
-        .cert(serviceAccount),
+        .cert({
+            projectId: process.env.PROJECT_ID,
+            privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            clientEmail: process.env.CLIENT_EMAIL,
+        }),
     databaseURL: process.env.realtimeDB
 });
 
