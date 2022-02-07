018e4886fef448bb5635e6b5250304b4c2d88244
dh8570 <72989583+dh8570@users.noreply.github.com>
2022-02-08 01:16:48 +0900

    #25

---
+++ functions/crawling/education.js
@@ -0,0 +1,58 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+exports.education = functions
+    .region('asia-northeast1')
+    .https
+    .onRequest((req, res) => {
+        axios
+            .get('https://www.sungkyul.ac.kr/computer/4104/subview.do')
+            .then(html => {
+                const tableCrawling = new Object();
+                const $ = cheerio.load(html.data);
+                const tableSize = $(
+                    '#menu4104_obj259 > div._fnctWrap > form:nth-child(2) > div > table > tbody > tr'
+                ).length - 4;
+
+                for (let index = 1; index < tableSize; index++) {
+                    tableCrawling[index] = {
+                        'title': $(
+                            '#menu4104_obj259 > div._fnctWrap > form:nth-child(2) > div > table > tbody > ' +
+                            'tr:nth-child(' + index + ') > td.td-subject'
+                        )
+                            .text()
+                            .trim(),
+                        'date': $(
+                           '#menu4104_obj259 > div._fnctWrap > form:nth-child(2) > div > table > tbody > ' +
+                           'tr:nth-child(' + index + ') > td.td-date'
+                        )
+                            .text()
+                            .trim(),
+                        'url': $(
+                            '#menu4104_obj259 > div._fnctWrap > form:nth-child(2) > div > table > tbody > ' +
+                            'tr:nth-child(' + index + ') > td.td-subject > a'
+                        )
+                            .attr('href')
+                            .replace(/^/, 'https://www.sungkyul.ac.kr')
+                    }
+                
+                }
+
+                return tableCrawling;
+            })
+            .then(async (result) => {
+                await admin
+                    .database()
+                    .ref('education/')
+                    .set(result);
+                console.log('education DB input Success');
+                res
+                    .status(201)
+                    .json(result);
+            })
+            .catch(error => {
+                console.error('Error from education : ', error);
+            });
+    });
---
+++ functions/test/crawling/education_spec.js
@@ -0,0 +1,68 @@
+const request = require('supertest');
+const {expect} = require('chai');
+require('dotenv').config();
+
+describe('GET /education', () => {
+    it('responds crawling type', done => {
+        request(process.env.crawlingUrl)
+            .get('/education')
+            .expect(201)
+            .then(res => {
+                expect(res.body)
+                    .to
+                    .be
+                    .an('object');
+                done();
+            })
+            .catch(err => {
+                console.error("Error >>", err);
+                done(err);
+            })
+        });
+    it('responds crawling length', done => {
+        request(process.env.crawlingUrl)
+            .get('/education')
+            .expect(201)
+            .then(res => {
+                expect(Object.keys(res.body).length)
+                    .to
+                    .equal(5)
+                done();
+            })
+            .catch(err => {
+                console.error("Error >>", err);
+                done(err);
+            })
+        });
+    it('responds crawling all right keys and values', done => {
+        request(process.env.crawlingUrl)
+            .get('/education')
+            .expect(201)
+            .then(res => {
+                for (let index = 1; index <= Object.keys(res.body).length; index++) {
+                    expect(res.body[index])
+                        .to
+                        .have
+                        .all
+                        .keys('title', 'date', 'url')
+                    expect(res.body[index].title)
+                        .to
+                        .be
+                        .a('string')
+                    expect(res.body[index].date)
+                        .to
+                        .be
+                        .a('string')
+                    expect(res.body[index].url)
+                        .to
+                        .be
+                        .a('string')
+                }
+                done();
+            })
+            .catch(err => {
+                console.error("Error >>", err);
+                done(err);
+            })
+        });
+});
