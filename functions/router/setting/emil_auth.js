b1e4e89d487d12d16469a68946d7722226409c62
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-02-28 23:57:07 +0900

    #47

--- functions/router/setting/emil_auth.js
+++ functions/router/setting/emil_auth.js
@@ -19,15 +19,10 @@ router.post('/', async function (req, res) {
             console.error('Error from auth to createUser:', e);
         });
 
-    // await admin
-    //     .auth()
-    //     .generateEmailVerificationLink(userRequest.email)
-    //     .then(async (result) => {
-    //         console.log(result);
-    //     })
-    //     .catch(e => {
-    //         console.error('Error from auth to generateEmailVerificationLink:', e);
-    //     });
+    // await admin     .auth()     .generateEmailVerificationLink(userRequest.email)
+    // .then(async (result) => {         console.log(result);     })     .catch(e =>
+    // {         console.error('Error from auth to generateEmailVerificationLink:',
+    // e);     });
 
     const firestore = admin.firestore();
     const docRef = firestore
@@ -35,36 +30,37 @@ router.post('/', async function (req, res) {
         .doc(userAbout.plusfriendUserKey);
     await docRef
         .set({email: email, grade: grade, studentID: studentID})
+        .then(() => {
+            const responseBody = {
+                version: "2.0",
+                template: {
+                    outputs: [
+                        {
+                            simpleText: {
+                                text: "프로필 생성이 완료되었습니다. 하단의 버튼을 통해 본인의 학점도 바로 입력해주세요!"
+                            }
+                        }
+                    ],
+                    quickReplies: [
+                        {
+                            "messageText": "학점 입력할게",
+                            "action": "block",
+                            "blockId": functions
+                                .config()
+                                .service_url
+                                .credit_key,
+                            "label": "학점 입력"
+                        }
+                    ]
+                }
+            };
+            res
+                .status(201)
+                .send(responseBody);
+        })
         .catch(e => {
             console.error('Error from set DB:', e);
         });
-
-    const responseBody = {
-        version: "2.0",
-        template: {
-            outputs: [
-                {
-                    simpleText: {
-                        text: "프로필 생성이 완료되었습니다. 하단의 버튼을 통해 본인의 학점도 바로 입력해주세요!"
-                    }
-                }
-            ],
-            quickReplies: [
-                {
-                    "messageText": "학점 입력할게",
-                    "action": "block",
-                    "blockId": functions
-                        .config()
-                        .service_url
-                        .credit_key,
-                    "label": "학점 입력"
-                }
-            ]
-        }
-    };
-    res
-        .status(201)
-        .send(responseBody);
 });
 
 module.exports = router;
