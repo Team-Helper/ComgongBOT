7a349452ab36d70c4fb9fe83b88c819a46de8dd1
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-03-25 19:18:32 +0900

    #31 - 학점, 학번 수정의 모카코드 주석처리

--- functions/test/router/setting/input/credit_modify_spec.js
+++ functions/test/router/setting/input/credit_modify_spec.js
@@ -2,31 +2,75 @@ const request = require('supertest');
 const {expect} = require('chai');
 const functions = require('firebase-functions');
 
-describe('POST /setting/credit_modify', () => {
-    const userRequest = {
-        user: {
-            "properties": {
-                "plusfriendUserKey": "some-id",
-                "isFriend": true
+describe('POST /setting/credit_modify', () => { // 테스트 수트
+    it('responds success modify credit', done => { // 테스트 단위(확인하고자 하는 내용을 명시)
+        const userRequest = { // 학점을 수정하는 사용자의 기본 정보 시나리오
+            user: {
+                "properties": {
+                    "plusfriendUserKey": "some-id", // 사용자 카카오 채널 아이디
+                    "isFriend": true // 채널 추가 상태
+                }
             }
-        }
-    };
-    const action = {
-        detailParams: {
-            menu: {
+        };
+        const action = { // 사용자가 선택 및 입력한 학점 수정 데이터 시나리오 (교과목, 학점)
+            detailParams: {
+                menu: {
+                    'groupName': '',
+                    'origin': '총 학점',
+                    'value': 'total'
+                },
+                credit: {
+                    'groupName': '',
+                    'origin': '12345',
+                    'value': '12345'
+                }
+            }
+        };
+        request(functions.config().service_url.app) // 테스트 하려는 기본 주소
+            .post('/setting/credit_modify') // 주소의 엔드포인트
+            .set('Accept', 'application/json')
+            .type('application/json')
+            .send({userRequest}) // body 데이터 전송(하단 포함)
+            .send({action})
+            .expect(201) // 응답 상태코드
+            .then(res => {
+                const element = res
+                    .body
+                    .template
+                    .outputs[0]
+                    .simpleText;
+                expect(element.text)
+                    .to
+                    .include('입력하신 학점으로 수정'); // 응답 결과가 작성한 텍스트 내용을 포함하는가
+                done();
+            })
+            .catch(err => {
+                console.error("Error >>", err);
+                done(err);
+            })
+        });
+
+    it('responds fail modify credit', done => {
+        const userRequest = {
+            user: {
+                "properties": {
+                    "plusfriendUserKey": "some-id",
+                    "isFriend": true
+                }
+            }
+        };
+        const action = {
+            detailParams: {
                 'groupName': '',
                 'origin': '총 학점',
                 'value': 'total'
             },
             credit: {
                 'groupName': '',
-                'origin': '15',
-                'value': '15'
+                'origin': '12345',
+                'value': '12345'
             }
-        }
-    }
-
-    it('responds try to same change credit', done => {
+        };
         request(functions.config().service_url.app)
             .post('/setting/credit_modify')
             .set('Accept', 'application/json')
@@ -51,60 +95,13 @@ describe('POST /setting/credit_modify', () => {
                 // console.log(elementQuick);
                 expect(elementQuick.messageText)
                     .to
-                    .include('나의 학점을 수정');
+                    .include('나의 학점을 수정'); // 응답 블록의 바로가기 요청문이 작성한 텍스트 내용을 포함하는가
+                expect(elementQuick.action)
+                    .to
+                    .equal('block'); // 응답 블록의 바로가기 구조가 블록 구조인가
                 expect(elementQuick.label)
                     .to
-                    .include('뒤로가기');
-                done();
-            })
-            .catch(err => {
-                console.error("Error >>", err);
-                done(err);
-            })
-        });
-});
-
-describe('POST /setting/credit_modify', () => {
-    const userRequest = {
-        user: {
-            "properties": {
-                "plusfriendUserKey": "some-id",
-                "isFriend": true
-            }
-        }
-    };
-    const action = {
-        detailParams: {
-            menu: {
-                'groupName': '',
-                'origin': '총 학점',
-                'value': 'total'
-            },
-            credit: {
-                'groupName': '',
-                'origin': '12345',
-                'value': '12345'
-            }
-        }
-    }
-
-    it('responds success change credit', done => {
-        request(functions.config().service_url.app)
-            .post('/setting/credit_modify')
-            .set('Accept', 'application/json')
-            .type('application/json')
-            .send({userRequest})
-            .send({action})
-            .expect(201)
-            .then(res => {
-                const element = res
-                    .body
-                    .template
-                    .outputs[0]
-                    .simpleText;
-                expect(element.text)
-                    .to
-                    .include('입력하신 학점으로 수정');
+                    .include('뒤로가기'); // 응답 블록의 바로가기 버튼명이 작성한 텍스트 내용을 포함하는가
                 done();
             })
             .catch(err => {
--- functions/test/router/setting/input/studentID_modify_spec.js
+++ functions/test/router/setting/input/studentID_modify_spec.js
@@ -2,17 +2,17 @@ const request = require('supertest');
 const {expect} = require('chai');
 const functions = require('firebase-functions');
 
-describe('POST /setting/setting_service', () => {
-    it('responds success modify studentID', done => {
-        const userRequest = {
+describe('POST /setting/setting_service', () => { // 테스트 수트
+    it('responds success modify studentID', done => { // 테스트 단위(확인하고자 하는 내용을 명시)
+        const userRequest = { // 학번을 변경하는 사용자의 기본 정보 시나리오
             user: {
                 "properties": {
-                    "plusfriendUserKey": "some-id",
-                    "isFriend": true
+                    "plusfriendUserKey": "some-id", // 사용자 카카오 채널 아이디
+                    "isFriend": true // 채널 추가 상태
                 }
             }
         };
-        const action = {
+        const action = { // 사용자가 입력한 학번 변경 데이터 시나리오
             detailParams: {
                 studentID_modify: {
                     groupName: '',
@@ -21,16 +21,13 @@ describe('POST /setting/setting_service', () => {
                 }
             }
         }
-
-        request(functions.config().service_url.app)
-            .post(
-                '/setting/studentID_correction'
-            )
+        request(functions.config().service_url.app) // 테스트 하려는 기본 주소
+            .post('/setting/studentID_correction') // 주소의 엔드포인트
             .set('Accept', 'application/json')
             .type('application/json')
-            .send({userRequest})
+            .send({userRequest}) // body 데이터 전송(하단 포함)
             .send({action})
-            .expect(201)
+            .expect(201) // 응답 상태코드
             .then(res => {
                 const element = res
                     .body
@@ -40,7 +37,7 @@ describe('POST /setting/setting_service', () => {
                 // console.log(element);
                 expect(element.text)
                     .to
-                    .include("입력하신 학번으로 변경");
+                    .include("입력하신 학번으로 변경"); // 응답 결과가 작성한 텍스트 내용을 포함하는가
                 done();
             })
             .catch(err => {
@@ -67,7 +64,6 @@ describe('POST /setting/setting_service', () => {
                 }
             }
         }
-
         request(functions.config().service_url.app)
             .post(
                 '/setting/studentID_correction'
@@ -86,21 +82,22 @@ describe('POST /setting/setting_service', () => {
                 // console.log(element);
                 expect(element.text)
                     .to
-                    .include("같은 학번");
-                const backElement = res
+                    .include("같은 학번"); // 응답 결과가 작성한 텍스트 내용을 포함하는가
+
+                const elementQuick = res
                     .body
                     .template
                     .quickReplies[0];
-                // console.log(backElement);
-                expect(backElement.messageText)
+                // console.log(elementQuick);
+                expect(elementQuick.messageText)
                     .to
-                    .equal('나의 학번을 변경할게');
-                expect(backElement.action)
+                    .equal('나의 학번을 변경할게'); // 응답 블록의 바로가기 요청문이 작성한 텍스트 내용을 포함하는가
+                expect(elementQuick.action)
                     .to
-                    .equal('block');
-                expect(backElement.label)
+                    .equal('block'); // 응답 블록의 바로가기 구조가 블록 구조인가
+                expect(elementQuick.label)
                     .to
-                    .include('뒤로가기');
+                    .include('뒤로가기'); // 응답 블록의 버튼명이 작성한 텍스트 내용을 포함하는가
                 done();
             })
             .catch(err => {
