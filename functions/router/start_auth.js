1cd532f9823f117d22ec3f533101013064c7d54d
dh8570 <72989583+dh8570@users.noreply.github.com>
2022-04-10 00:05:26 +0900

    Revert "Merge branch 'dh8570_New' of https://github.com/Team-Helper/ComgongBOT into dh8570_New"
    This reverts commit 92b7ac79003e87e745b68960898118f17373cbee, reversing
    changes made to ca9ff7372c56e791a88e0d9fea1a87c248ef996f.

---
+++ functions/router/start_auth.js
@@ -0,0 +1,73 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+async function checkAuth(req) {
+    // console.log(req);
+    let responseBody;
+
+    if (req.isFriend == undefined) { // 채널을 추가하지 않은 경우
+        responseBody = {
+            version: "2.0",
+            template: {
+                outputs: [
+                    {
+                        simpleText: {
+                            text: "🔕 컴공봇 채널 추가부터 하셔야 이용이 가능해요.!" // 텍스트 뷰 블록으로 출력
+                        }
+                    }
+                ]
+            }
+        };
+    } else {
+        /*사용자 프로필 DB 조회*/
+        const firestore = admin.firestore();
+        const userSelect = firestore
+            .collection('users')
+            .doc(req.plusfriendUserKey);
+        const userData = await userSelect.get();
+
+        if (!userData.exists) { // 채널은 추가 했으나 프로필 DB가 없는 경우
+            console.log('No such user!');
+            const title = ["이메일", "학년/학번"];
+            const description = "❌ 미설정";
+            const itemList = [];
+
+            title.forEach(value => {
+                itemList.push({"title": value, "description": description});
+            });
+            responseBody = {
+                version: "2.0",
+                template: {
+                    outputs: [
+                        {
+                            itemCard: { // 아이템 카드 뷰 블록으로 출력
+                                "head": {
+                                    "title": "⚠ 누락된 설정이 있습니다."
+                                },
+                                "itemList": itemList,
+                                "title": "컴공봇 이용을 위해 이메일 인증과 학년/학번 입력은 필수 입니다."
+                            }
+                        }
+                    ],
+                    quickReplies: [
+                        { // 바로가기 작성 및 출력 설정
+                            "messageText": "이메일 인증할게",
+                            "action": "block",
+                            "blockId": functions
+                                .config()
+                                .service_url
+                                .email_key,
+                            "label": "이메일 인증"
+                        }
+                    ]
+                }
+            };
+        } else { // 프로필 DB가 존재하는 경우
+            // console.log('user data:', userData.data());
+            return true; // 참 값을 반환
+        }
+    }
+    return responseBody; // 작성된 누락 설정 관련 내용 리턴
+}
+
+module.exports = checkAuth;
---
+++ functions/test/router/personal/personalhub_spec.js
@@ -0,0 +1,241 @@
+const request = require('supertest');
+const {expect} = require('chai');
+const functions = require('firebase-functions');
+
+describe('POST /personal', () => { // 테스트 수트
+    it('responds about isFriend is false', done => { //테스트 단위 : 채널 추가가 안되어있을 떄
+        const userRequest = { // 기본 사용자 정보 시나리오
+            user: {
+                "properties": {
+                    "plusfriendUserKey": "some-id", // 사용자 카카오 채널 아이디
+                    "isFriend": undefined // 채널 추가 상태
+                }
+            }
+        };
+        request(functions.config().service_url.app) // 테스트 하려는 기본 주소
+            .post('/personal') // 주소의 엔드포인트
+            .set({
+                key: functions
+                    .config()
+                    .service_url
+                    .personal_key
+            })
+            .set('Accept', 'application/json')
+            .type('application/json')
+            .send({userRequest}) // body 데이터 전송
+            .expect(201) // 응답 상태코드
+            .then(res => {
+                const element = res
+                    .body
+                    .template
+                    .outputs[0]
+                    .simpleText;
+                // console.log(element);
+                expect(element.text)
+                    .to
+                    .include("컴공봇 채널 추가부터"); // 응답 결과가 작성한 텍스트 내용을 포함하는가
+                done();
+            })
+            .catch(err => {
+                console.error("Error >>", err);
+                done(err);
+            })
+        });
+
+    it(
+        'responds isFriend is true and auth fail',
+        done => { // 채널은 추가되었으나 프로필 인증이 안되어있을 때
+            const userRequest = {
+                user: {
+                    "properties": {
+                        "plusfriendUserKey": "some-id",
+                        "isFriend": true
+                    }
+                }
+            };
+            request(functions.config().service_url.app)
+                .post('/personal')
+                .set({
+                    key: functions
+                        .config()
+                        .service_url
+                        .personal_key
+                })
+                .set('Accept', 'application/json')
+                .type('application/json')
+                .send({userRequest})
+                .expect(201)
+                .then(res => {
+                    const element = res
+                        .body
+                        .template
+                        .outputs[0]
+                        .itemCard;
+                    // console.log(element);
+                    expect(element.head.title)
+                        .to
+                        .be
+                        .a('string'); // 아이템 카드 뷰의 제목이 문자열 타입인가
+                    expect(element.head.title)
+                        .to
+                        .include('누락된 설정이'); // 아이템 카드 뷰의 제목 내용이 작성한 텍스트 내용을 포함하는가
+                    expect(element.title)
+                        .to
+                        .equal('컴공봇 이용을 위해 이메일 인증과 학년/학번 입력은 필수 입니다.'); // 아이템 카드 뷰의 설명 내용이 작성한 텍스트 내용과 완전 일치하는가
+
+                    const elementItems = element.itemList;
+                    const title = ['이메일', '학년/학번', '학점'];
+                    for (let index = 0; index < elementItems.length; index++) {
+                        const itemTitle = elementItems[index].title;
+                        const itemDescription = elementItems[index].description;
+                        expect(itemTitle)
+                            .to
+                            .equal(title[index]); // 아이템 카드 뷰의 본문 구성이 지정한 배열 내용과 완전 일치하는가
+                        expect(itemDescription)
+                            .to
+                            .include('미설정'); // 아이템 카드 뷰의 본문 내용이 작성한 텍스트 내용을 포함하는가
+                    }
+
+                    const elementQuick = res
+                        .body
+                        .template
+                        .quickReplies[0];
+                    // console.log(element);
+                    expect(elementQuick.messageText)
+                        .to
+                        .equal('이메일 인증할게'); // 응답 블록의 바로가기 요청문 내용이 작성한 텍스트 내용과 완전 일치하는가
+                    expect(elementQuick.label)
+                        .to
+                        .equal('이메일 인증'); // 응답 블록의 바로가기 버튼명이 작성한 텍스트 내용과 완전 일치하는가
+                    done();
+                })
+                .catch(err => {
+                    console.error("Error >>", err);
+                    done(err);
+                })
+            }
+    );
+
+    it('responds credits DB fail', done => { // 프로필 DB에 학점 데이터가 없을 때
+        const userRequest = {
+            user: {
+                "properties": {
+                    "plusfriendUserKey": "some-id",
+                    "isFriend": true
+                }
+            }
+        };
+        request(functions.config().service_url.app)
+            .post('/personal')
+            .set({
+                key: functions
+                    .config()
+                    .service_url
+                    .personal_key
+            })
+            .set('Accept', 'application/json')
+            .type('application/json')
+            .send({userRequest})
+            .expect(201)
+            .then(res => {
+                const element = res
+                    .body
+                    .template
+                    .outputs[0]
+                    .itemCard;
+                // console.log(element);
+                expect(element.head.title)
+                    .to
+                    .be
+                    .a('string');
+                expect(element.head.title)
+                    .to
+                    .include('누락된 설정이');
+                expect(element.title)
+                    .to
+                    .equal('학과 개인 서비스는 학점 입력이 완료되어야 이용이 가능해집니다.');
+
+                const elementItems = element.itemList;
+                const title = ["전공필수", "전공선택", "교양필수", "교양선택", "총 학점"];
+                for (let index = 0; index < elementItems.length; index++) {
+                    const itemTitle = elementItems[index].title;
+                    const itemDescription = elementItems[index].description;
+                    expect(itemTitle)
+                        .to
+                        .equal(title[index]);
+                    expect(itemDescription)
+                        .to
+                        .include('미설정');
+                }
+
+                const elementQuick = res
+                    .body
+                    .template
+                    .quickReplies[0];
+                // console.log(element);
+                expect(elementQuick.messageText)
+                    .to
+                    .equal('학점 입력할게');
+                expect(elementQuick.label)
+                    .to
+                    .equal('학점 입력');
+                done();
+            })
+            .catch(err => {
+                console.error("Error >>", err);
+                done(err);
+            })
+        });
+
+    it('responds auth success', done => { // 프로필과 학점 입력까지 완료되어있을 때
+        const userRequest = {
+            user: {
+                "properties": {
+                    "plusfriendUserKey": "some-id",
+                    "isFriend": true
+                }
+            }
+        };
+        request(functions.config().service_url.app)
+            .post('/personal')
+            .set({
+                key: functions
+                    .config()
+                    .service_url
+                    .personal_key
+            })
+            .set('Accept', 'application/json')
+            .type('application/json')
+            .send({userRequest})
+            .expect(201)
+            .then(res => {
+                const element = res
+                    .body
+                    .template
+                    .outputs[0]
+                    .simpleText;
+                // console.log(element);
+                expect(element.text)
+                    .to
+                    .be
+                    .a('string'); // 응답 블록의 본문이 문자열 타입인가
+                expect(element.text)
+                    .to
+                    .include('원하시는 학과 메뉴를 선택'); // 응답 블록의 본문이 작성한 텍스트 내용을 포함하는가
+
+                const elementQuick = res.body.template.quickReplies;
+                // console.log(element);
+                const array = ['학점 조회', '졸업학점 계산', '졸업이수 조건 확인'];
+                for (let index = 0; index < elementQuick.length; index++) {
+                    expect(elementQuick[index].label)
+                        .to
+                        .include(array[index]); // 응답 블록의 바로가기 버튼명이 지정한 배열 내용을 포함하는가
+                }
+                done();
+            })
+            .catch(err => {
+                console.error("Error >>", err);
+                done(err);
+            })
+        });
+});
