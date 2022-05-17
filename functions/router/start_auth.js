25d09dccf5e3fee863727008d572580f552d9885
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-05-17 17:22:28 +0900

    Update start_auth.js

--- functions/router/start_auth.js
+++ functions/router/start_auth.js
@@ -30,7 +30,7 @@ async function checkAuth(req) {
             const title = ["이메일", "학년/학번"];
             const description = "❌ 미설정";
             const itemList = [];
-            const url = 'https://comgong-bot.web.appemail?variable=';
+            const url = 'https://comgong-bot.web.app/email?variable=';
             const newURL = new URL(url);
             newURL
                 .searchParams
