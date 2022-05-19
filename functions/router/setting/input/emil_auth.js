04b4d2db728d02ca48262e8733275aac20a5bc95
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-05-20 01:52:24 +0900

    Update emil_auth.js

--- functions/router/setting/input/emil_auth.js
+++ functions/router/setting/input/emil_auth.js
@@ -5,7 +5,7 @@ const CryptoJS = require('crypto-js');
 const functions = require('firebase-functions');
 
 router.post('/', async function (req, res) {
-    // console.log(req.body);
+    console.log(req.body);
     const userRequest = req.body.Data; // 입력한 사용자의 데이터 조회
     const email = userRequest.email; // 입력한 이메일
     const grade = userRequest.grade; // 입력한 학년
@@ -15,7 +15,7 @@ router.post('/', async function (req, res) {
         .AES
         .decrypt(userRequest.userKey, functions.config().service_key.aes_key);
     const decrypted = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-    // console.log(email, grade, studentID, decrypted);
+    console.log(email, grade, studentID, decrypted);
 
     /* 사용자 카카오채널 id 값으로 프로필 DB 생성 */
     const firestore = admin.firestore();
