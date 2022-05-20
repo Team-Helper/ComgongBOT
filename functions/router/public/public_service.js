8914aacb24bcc004df91284d8a3c627faf9e55fc
dh8570 <72989583+dh8570@users.noreply.github.com>
2022-05-21 04:26:18 +0900

    #91

--- functions/router/public/public_service.js
+++ functions/router/public/public_service.js
@@ -238,16 +238,16 @@ router.post('/', async function (req, res) {
             {
                 image = await getImg('completionSystem');
                 // console.log(image);
-                const imgTitle = ['올해 이수체계도', '올해 설계-이수체계도'];
                 /* 응답 횟수만큼 이미지 블록 뷰를 생성*/
-                image.forEach((value, index) => {
+                image.forEach((value) => {
                     items.push({
                         simpleImage: {
-                            "imageUrl": value,
-                            "altText": imgTitle[index]
+                            "imageUrl": value.imgURL,
+                            "altText": value.imgAlt
                         }
                     });
                 });
+
                 // console.log(items);
                 responseBody = {
                     version: "2.0",
--- functions/test/crawling/completion_system_spec.js
+++ functions/test/crawling/completion_system_spec.js
@@ -12,20 +12,32 @@ describe('GET /completionSystem', () => { // 테스트 수트
                 console.log(element);
                 expect(element)
                     .to
-                    .have
-                    .lengthOf(2); // 응답 결과 개수가 지정한 값 만큼인가
-                for (let index = 0; index < element.length; index++) {
+                    .be
+                    .an('object'); // 응답 결과가 오브젝트 타입인가
+                expect(Object.keys(element).length)
+                    .to
+                    .equal(2); // 응답 결과의 갯수가 지정한 값 만큼인가
+                for (let index = 0; index < Object.keys(res.body).length; index++) {
                     const thisYear = new Date().getFullYear();
                     expect(element[index])
                         .to
+                        .have
+                        .all
+                        .keys('imgURL', 'imgAlt'); // 응답 결과가 지정한 key 값을 지니고 있는가
+                    expect(element[index].imgURL)
+                        .to
                         .be
                         .a('string'); // 응답 결과가 문자열 타입인가
-                    expect(element[index])
+                    expect(element[index].imgURL)
                         .to
                         .include('jpg'); // 응답 결과가 jpg 파일명인가
-                    expect(element[index])
+                    expect(element[index].imgURL)
                         .to
                         .include(thisYear); // 응답 결과가 올해 날짜가 적힌 내용을 포함하는가
+                    expect(element[index].imgAlt)
+                        .to
+                        .be
+                        .a('string'); // 응답 결과가 문자열 타입인가
                 }
                 done();
             })
