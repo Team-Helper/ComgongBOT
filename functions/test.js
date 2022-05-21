d72ddff3cd5cdac06165a910219fe89efd5bae89
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-05-22 00:14:13 +0900

    Update test.js

--- functions/test.js
+++ functions/test.js
@@ -4,12 +4,9 @@ const router = express.Router();
 router.post('/', async function (req, res) {
     // const userAbout = req.body.userRequest.user.properties; // 사용자 카카오 채널 정보
     // console.log(userAbout);
-    console.log(req.body);
-    const userRequest = req.body.action; // 사용자 입력 데이터
-    console.log(userRequest);
-
-    const test = userRequest.test.value; // 입력한 전공필수
-    console.log(typeof test);
+    // console.log(req.body);
+    const userRequest = req.body.utterance; // 사용자 입력 데이터
+    console.log(userRequest, typeof userRequest);
     if (typeof test === 'string') {
         res.sendStatus(200);
     } else {
