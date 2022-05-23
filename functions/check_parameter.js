bcf4acfcc2dbae641f091583c128b6b9dad608fb
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-05-23 13:54:51 +0900

    Update check_parameter.js

--- functions/check_parameter.js
+++ functions/check_parameter.js
@@ -4,8 +4,8 @@ exports.checkParameter = functions // 함수 이름
     .region('asia-northeast1')
     .https
     .onRequest(async (req, res) => {
-        // console.log(req.body);
-        const isNumber = parseInt(req.body.userRequest.utterance);
+        console.log(req.body);
+        const isNumber = parseInt(req.body.utterance);
         console.log(isNumber);
         if (!isNaN(isNumber)) {
             res
