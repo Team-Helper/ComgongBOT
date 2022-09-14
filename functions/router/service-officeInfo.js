1b078a9bb017bf4c25113449c540f129264b6856
admin <dh8570@naver.com>
2022-09-14 22:19:58 +0900

    #118

--- functions/router/service-officeInfo.js
+++ functions/router/service-officeInfo.js
@@ -11,30 +11,18 @@ router.post('/', async function (req, res) {
     /* 각 게시물 값 저장*/
     let address,
         tel; // 교수진 소개 정보와 이름 저장
-    let items = []; // 게시판 별 value 저장
 
     if (checkAuth === true) { // 사용자가 프로필 설정이 되어있다면
         [address, tel] = await getData('officeInfo'); // DB로부터 해당 게시물의 데이터 get
-        /* 리스트 카드 뷰 본문 작성*/
-        items.push({"address": address, "tel": tel});
-        // console.log(items);
+
         responseBody = {
             version: "2.0",
             template: {
                 outputs: [
                     {
-                        itemCard: { // 아이템 카드 뷰 블록으로 출력
-                            "header": {
-                                "title": "학과 사무실 정보"
-                            },
-                            "itemList": items,
-                            "buttons": [
-                                { // 해당 페이지 바로이동 관련 하단 버튼 생성
-                                    "label": "학과 홈페이지",
-                                    "action": "webLink",
-                                    "webLinkUrl": "https://www.sungkyul.ac.kr/sites/computer/index.do"
-                                }
-                            ]
+                        simpleText: {
+                            text: `학과 사무실 주소: ${address}
+                            학과 사무실 연락처: ${tel}`
                         }
                     }
                 ]
