91df74e56a4deb8ab889651b2eef1c127a788bb0
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-10-30 22:56:17 +0900

    Update check-number.js

--- functions/check-number.js
+++ functions/check-number.js
@@ -5,21 +5,15 @@ exports.checkNumber = functions
     .https
     .onRequest(async (req, res) => {
         /* 사용자가 입력한 학점 값이 숫자형 타입인지를 검증 */
-        console.log(req.body.utterance);
-        if (req.body.utterance === "입력 취소") {
-            console.log('hello?');
-            res.send({"status": "IGNORE"});
+        const isNumber = parseInt(req.body.utterance);
+        // console.log(isNumber, typeof isNumber);
+        if (!isNaN(isNumber)) {
+            res
+                .status(200)
+                .send({"status": "SUCCESS"});
         } else {
-            const isNumber = parseInt(req.body.utterance);
-            console.log(isNumber, typeof isNumber);
-            if (!isNaN(isNumber)) {
-                res
-                    .status(200)
-                    .send({"status": "SUCCESS"});
-            } else {
-                res
-                    .status(400)
-                    .send({"status": "FAIL"});
-            }
+            res
+                .status(400)
+                .send({"status": "FAIL"});
         }
     });
