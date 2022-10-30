61cc21a5871f18bcfeec18c7c5637f69bce72c88
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-10-30 22:00:06 +0900

    검증 API - 입력 취소 예외처리

--- functions/check-number.js
+++ functions/check-number.js
@@ -5,15 +5,19 @@ exports.checkNumber = functions
     .https
     .onRequest(async (req, res) => {
         /* 사용자가 입력한 학점 값이 숫자형 타입인지를 검증 */
-        const isNumber = parseInt(req.body.utterance);
-        // console.log(isNumber, typeof isNumber);
-        if (!isNaN(isNumber)) {
-            res
-                .status(200)
-                .send({"status": "SUCCESS"});
+        if (req.body.utterance === "입력 취소") {
+            res.send({"status": "IGNORE"});
         } else {
-            res
-                .status(400)
-                .send({"status": "FAIL"});
+            const isNumber = parseInt(req.body.utterance);
+            // console.log(isNumber, typeof isNumber);
+            if (!isNaN(isNumber)) {
+                res
+                    .status(200)
+                    .send({"status": "SUCCESS"});
+            } else {
+                res
+                    .status(400)
+                    .send({"status": "FAIL"});
+            }
         }
     });
